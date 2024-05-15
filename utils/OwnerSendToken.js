39d725376e5e89dcf8c461950ac602acbc7cf6a7
megharji <meghareji25@gmail.com>
2024-05-15 20:54:45 +0530

    token update

--- utils/OwnerSendToken.js
+++ utils/OwnerSendToken.js
@@ -1,13 +1,13 @@
-exports.Ownersendtoken = (owner, statusCode, res) => { 
-    const Ownertoken = owner.getjwttoken()
+exports.Ownersendtoken = (owner, statusCode, res) => {
+    const Ownertoken = owner.getjwttoken();
     const options = {
         expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRE *24*60*60*1000 
+            Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true,
+        secure: true,
+        sameSite: "None"
+    };
 
-    }
-
-    res.status(statusCode).cookie("Ownertoken", Ownertoken, options).json({success:true, id: owner._id, Ownertoken})  
-    res.json({Ownertoken});
-}
+    res.cookie("jwt", Ownertoken, options);
+    res.status(statusCode).json({ success: true, id: owner._id, Ownertoken });
+};
